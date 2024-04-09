b577c707cc92b2012349819db9c16bf8250112b9
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2024-04-09 22:01:57 +0900

    rename methods

---
+++ src/StringExKanaToHiragana.cs
@@ -0,0 +1,60 @@
+﻿namespace MyNihongo.KanaConverter;
+
+public static partial class StringEx
+{
+	/// <summary>
+	/// Converts a kana (hiragana or katakana) string to hiragana.
+	/// </summary>
+	/// <param name="this">Kana string to be converted to hiragana.</param>
+	/// <param name="unrecognisedCharacterPolicy">Behaviour how unrecognised characters are treated.</param>
+	/// <param name="stringBuilderPool">String builder pool that is useful when many strings are converted in a loop.</param>
+	/// <exception cref="InvalidKanaException"></exception>
+	public static string KanaToHiragana(this string @this, UnrecognisedCharacterPolicy unrecognisedCharacterPolicy = default, ObjectPool<StringBuilder>? stringBuilderPool = null)
+	{
+		var result = @this.ConvertKanaToHiragana(unrecognisedCharacterPolicy, stringBuilderPool);
+
+		if (result.ErrorMessage != null)
+			throw new InvalidKanaException(result.ErrorMessage);
+
+		return result.Value;
+	}
+
+	/// <summary>
+	/// Tries to convert a kana (hiragana or katakana) string to hiragana.
+	/// </summary>
+	/// <param name="this">Kana string to be converted to hiragana.</param>
+	/// <param name="value">Romaji string after conversion.</param>
+	public static bool TryConvertKanaToHiragana(this string @this, out string value) =>
+		@this.TryConvertKanaToHiragana(unrecognisedCharacterPolicy: default, stringBuilderPool: null, out value);
+
+	/// <summary>
+	/// Tries to convert a kana (hiragana or katakana) string to hiragana.
+	/// </summary>
+	/// <param name="this">Kana string to be converted to hiragana.</param>
+	/// <param name="unrecognisedCharacterPolicy">Behaviour how unrecognised characters are treated.</param>
+	/// <param name="value">Romaji string after conversion.</param>
+	public static bool TryConvertKanaToHiragana(this string @this, UnrecognisedCharacterPolicy unrecognisedCharacterPolicy, out string value) =>
+		@this.TryConvertKanaToHiragana(unrecognisedCharacterPolicy, stringBuilderPool: null, out value);
+
+	/// <summary>
+	/// Tries to convert a kana (hiragana or katakana) string to hiragana.
+	/// </summary>
+	/// <param name="this">Kana string to be converted to hiragana.</param>
+	/// <param name="unrecognisedCharacterPolicy">Behaviour how unrecognised characters are treated.</param>
+	/// <param name="stringBuilderPool">String builder pool that is useful when many strings are converted in a loop.</param>
+	/// <param name="value">Romaji string after conversion.</param>
+	public static bool TryConvertKanaToHiragana(this string @this, UnrecognisedCharacterPolicy unrecognisedCharacterPolicy, ObjectPool<StringBuilder>? stringBuilderPool, out string value)
+	{
+		var result = @this.ConvertKanaToHiragana(unrecognisedCharacterPolicy, stringBuilderPool);
+		value = result.Value;
+
+		if (unrecognisedCharacterPolicy == UnrecognisedCharacterPolicy.Append)
+			return result.ErrorMessage == null && value != @this;
+
+		return result.ErrorMessage == null;
+	}
+
+	private static ConversionResult ConvertKanaToHiragana(this string @this, UnrecognisedCharacterPolicy unrecognisedCharacterPolicy, ObjectPool<StringBuilder>? stringBuilderPool)
+	{
+	}
+}
