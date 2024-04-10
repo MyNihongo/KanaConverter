f0275a94de4ff0adc03d9bf3082c2d859a99e02f
kuzmins-85 <kuzmins-85@soft-gear.co.jp>
2024-04-10 17:37:08 +0900

    tests for convert

---
+++ tests/StringExKanaToHiraganaTests/KanaToHiraganaUnknownCharShould.cs
@@ -0,0 +1,46 @@
+﻿namespace MyNihongo.KanaConverter.Tests.StringExKanaToHiraganaTests;
+
+public sealed class KanaToHiraganaUnknownCharShould
+{
+	[Fact]
+	public void ThrowExceptionByDefault()
+	{
+		const string input = "ひaらがな";
+
+		var func = () => input.KanaToHiragana();
+
+		func
+			.Should()
+			.Throw<InvalidKanaException>();
+	}
+
+	[Fact]
+	public void SkipUnknownCharacters()
+	{
+		const UnrecognisedCharacterPolicy policy = UnrecognisedCharacterPolicy.Skip;
+
+		const string input = "ヒaラガナ",
+			expected = "ひらがな";
+
+		var result = input.KanaToHiragana(policy);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+
+	[Fact]
+	public void AppendUnknownCharacters()
+	{
+		const UnrecognisedCharacterPolicy policy = UnrecognisedCharacterPolicy.Append;
+
+		const string input = "ヒラガナガ好キ",
+			expected = "ひらがなが好き";
+
+		var result = input.KanaToHiragana(policy);
+
+		result
+			.Should()
+			.Be(expected);
+	}
+}
