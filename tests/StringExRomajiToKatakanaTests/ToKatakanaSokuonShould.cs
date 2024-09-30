b0514a5ad594bdcf369d7321125d915098346ef3
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2024-09-30 09:38:49 +0900

    tests for n

--- tests/StringExRomajiToKatakanaTests/ToKatakanaSokuonShould.cs
+++ tests/StringExRomajiToKatakanaTests/ToKatakanaSokuonShould.cs
@@ -3,7 +3,7 @@ namespace MyNihongo.KanaConverter.Tests.StringExRomajiToKatakanaTests;
 public sealed class ToKatakanaSokuonShould
 {
 	[Fact]
-	public void ReturnCharsSokuonN()
+	public void ReturnCharsSokuonNSingle()
 	{
 		const string input = "nn",
 			expected = "ンン";
@@ -87,6 +87,19 @@ public sealed class ToKatakanaSokuonShould
 	{
 		const string input = "ddajjizzuddeddo",
 			expected = "ッダッジッズッデッド";
+
+		var result = input.ToKatakana();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+	
+	[Fact]
+	public void ReturnCharsSokuonN()
+	{
+		const string input = "nnanninnunnennonnyannyinnyunnyennyo",
+			expected = "ンナンニンヌンネンノンニャンニィンニュンニェンニョ";
 
 		var result = input.ToKatakana();
 
