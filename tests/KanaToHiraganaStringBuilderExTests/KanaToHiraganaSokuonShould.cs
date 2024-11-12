9fca283cdc424d9aea6cfda97a53a27cc160d41e
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2024-11-12 22:00:55 +0900

    add tests for KanaToHiragana

---
+++ tests/KanaToHiraganaStringBuilderExTests/KanaToHiraganaSokuonShould.cs
@@ -0,0 +1,18 @@
+﻿namespace MyNihongo.KanaConverter.Tests.KanaToHiraganaStringBuilderExTests;
+
+public sealed class KanaToHiraganaSokuonShould
+{
+	[Fact]
+	public void ReturnCharsSokuon()
+	{
+		const string input = "ッン",
+			expected = "っん";
+
+		var result = new StringBuilder(input)
+			.KanaToHiragana();
+
+		result
+			.Should()
+			.Be(expected);
+	}
+}
