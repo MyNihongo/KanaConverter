732970be07a06a168eec76bd8ce76b891c324ff1
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2025-12-14 16:39:20 +0900

    to katakana

--- src/KanaToKatakanaEx.cs
+++ src/KanaToKatakanaEx.cs
@@ -55,6 +55,23 @@ public static class KanaToKatakanaEx
 
 			return result.ErrorMessage == null;
 		}
+
+		/// <summary>
+		/// Converts a kana (hiragana or katakana) string to katakana and appends it to the string builder.
+		/// </summary>
+		/// <param name="value">Value to convert to hiragana.</param>
+		/// <param name="unrecognisedCharacterPolicy">Behaviour how unrecognised characters are treated.</param>
+		/// <returns>String builder instance</returns>
+		public StringBuilder AppendKanaToKatakana(string? value, UnrecognisedCharacterPolicy unrecognisedCharacterPolicy = default)
+		{
+			var errorMessage = new StringTextContainer(value)
+				.AppendConvertKanaToKatakana(unrecognisedCharacterPolicy, @this);
+
+			if (!string.IsNullOrEmpty(errorMessage))
+				throw new InvalidCharacterException(errorMessage);
+
+			return @this;
+		}
 	}
 
 	/// <param name="this">Kana string to be converted to katakana.</param>
